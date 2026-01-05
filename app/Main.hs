b325bc5409324f6de544cc77f13de5e9b6ea9f7e
Brian Johnson <bjthinks@gmail.com>
2026-01-05 01:28:44 -0800

    Scramble the puzzle

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,11 @@ import Control.Exception (bracket)
 import Control.Lens
 import Control.Monad
 import Control.Monad.Trans.Maybe
+import Control.Monad.Random.Strict
 import Control.Monad.Reader
-import Control.Monad.State
+import Control.Monad.State.Strict
 import Data.Array
+import Data.Time.Clock.System (getSystemTime, SystemTime(..))
 import Graphics.Vty
 import Graphics.Vty.CrossPlatform
 
@@ -26,30 +28,67 @@ type Board = Array Coord Int
 data GameState = GameState
   { _terminalSize :: Coord
   , _board :: Board
+  , _blank :: Coord
   }
 
 $(makeLenses ''GameState)
 
-type Game = MaybeT (StateT GameState (ReaderT Environment IO))
+type Game = MaybeT (RandT StdGen (StateT GameState (ReaderT Environment IO)))
 
 solvedBoard :: Coord -> Board
 solvedBoard bs = listArray ((1, 1), bs) $ [1..(fst bs * snd bs - 1)] ++ [0]
 
+isValidPosition :: Coord -> Game Bool
+isValidPosition (yy, xx) = do
+  (ymax, xmax) <- view boardSize
+  return $ yy >= 1 && yy <= ymax && xx >= 1 && xx <= xmax
+
+randomMove :: Game ()
+randomMove = do
+  oldBoard <- use board
+  oldBlank <- use blank
+  let (oldBlankY, oldBlankX) = oldBlank
+  possibilities <- filterM isValidPosition
+    [(oldBlankY-1, oldBlankX), (oldBlankY, oldBlankX-1),
+     (oldBlankY+1, oldBlankX), (oldBlankY, oldBlankX+1)]
+  which <- lift $ getRandomR (0, length possibilities - 1)
+  let newBlank = possibilities !! which
+      tile = oldBoard ! newBlank
+      newBoard = oldBoard // [(oldBlank, tile), (newBlank, 0)]
+  board .= newBoard
+  blank .= newBlank
+
+scramblePuzzle :: Game ()
+scramblePuzzle = do
+  bs <- view boardSize
+  -- This is good for up to a 16x16 puzzle
+  let two = 2 :: Int
+  sequence_ $ replicate (4 * (fst bs)^two * (snd bs)^two) randomMove
+
 playGame :: Game String
 playGame = do
-  liftIO $ threadDelay 3000000
+  scramblePuzzle
+  liftIO $ threadDelay 1000000
   b <- use board
   return $ show b
+
+getNanosSinceEpoch :: IO Integer
+getNanosSinceEpoch = do
+  (MkSystemTime s ns) <- getSystemTime
+  return $ toInteger s * 10^(9 :: Int) + toInteger ns
 
 startGame :: Coord -> Vty -> IO (Maybe String)
 startGame bs v = do
   let env = Environment { _vty = v, _boardSize = bs }
+  t <- getNanosSinceEpoch
+  let gen = mkStdGen $ fromInteger t
   db <- displayBounds $ outputIface v
   let startState = GameState
         { _terminalSize = db
         , _board = solvedBoard bs
+        , _blank = bs
         }
-  runReaderT (evalStateT (runMaybeT playGame) startState) env
+  runReaderT (evalStateT (evalRandT (runMaybeT playGame) gen) startState) env
 
 main :: IO ()
 main = do
